e26df96c750ee8ae3786911e05462cc1991df348
Andrey Astahow <146195135+AndreyAstahow@users.noreply.github.com>
2024-07-10 20:15:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware, db
 from random import randint
-from io import BytesIO
-from PIL import Image
+from dotenv import load_dotenv
+
+from schema import Image as SchemaImage
+from models import ImageInfo as ModelImageInfo
 
 import uvicorn
-import requests
+import os
+
+load_dotenv('.env')
 
 app = FastAPI()
 
-@app.get('/url')
-async def test(url: str):
-    response = requests.get(url)
-    img_data = response.content
-    image = Image.open(BytesIO(img_data))
-    width, height = image.size
-    info = { 'width': width, 'height': height}
-    return {
-        'url' : f'{url}',
-        'id' : randint(100000, 999999),
-        'info' : info,
-    }
+app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
+
+def tags():
+    all_tags = ['animal', 'cars', 'tech', 'посуда', 'млекопитающие', 'одежда', 'мебель', 'пингвин', 'лошадь', 'программа', 'искусство', 'стол', 'стул', 'дед мороз', 'праздник', 'деньги']
+    tags = []
+    random_number = randint(0, 4)
+    while len(tags) < random_number:
+        random_tags = all_tags[randint(0, 15)]
+        if random_tags in tags:
+            continue
+        else:
+            tags.append(random_tags)
+    return tags
+
+@app.post('/load_image/')
+async def load_image(image: SchemaImage):
+    image_tags = tags()
+    db_image = ModelImageInfo(url = image.url, tags = image_tags)
+    db.session.add(db_image)
+    db.session.commit()
+    return db_image
+
+@app.get('/get_image/')
+async def get_image():
+    image = db.session.query(ModelImageInfo).all()
+    return image
+
+@app.get('/get_image/{id}')
+async def get_image_id(id: int):
+    image = db.session.query(ModelImageInfo).filter(ModelImageInfo.id == id).first()
+    return image
+
+@app.get('/get_url_by_tags/')
+async def get_url_by_tags(tags: str):
+    image = db.session.query(ModelImageInfo).filter(ModelImageInfo.tags == tags).all()
+    return image
+
+@app.get('/get_image_by_tags_and_id/')
+async def get_image_by_tags_and_id(id: int, tags: str):
+    image_dict = []
+    image_id = db.session.query(ModelImageInfo).filter(ModelImageInfo.id == id).all()
+    image_tags = db.session.query(ModelImageInfo).filter(ModelImageInfo.tags == tags).all()
+    image_dict.append(image_id)
+    image_dict.append(image_tags)
+    return image_dict
+
+@app.get('/get_id_by_tags/')
+async def get_id_by_tags(tags: str):
+    image = db.session.query(ModelImageInfo).filter(ModelImageInfo.tags == tags).all()
+    return image
 
 if __name__ == '__main__':
     uvicorn.run(app)
