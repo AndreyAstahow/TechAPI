81e684b02a65e31c0e009d469561c6e131eff587
Andrey Astahow <146195135+AndreyAstahow@users.noreply.github.com>
2024-07-16 20:41:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
 def tags():
     all_tags = ['animal', 'cars', 'tech', 'посуда', 'млекопитающие', 'одежда', 'мебель', 'пингвин', 'лошадь', 'программа', 'искусство', 'стол', 'стул', 'дед мороз', 'праздник', 'деньги']
-    tags = random.sample(all_tags, random.randint(0, 4))
-    return tags
+    return sample(all_tags, randint(0, 4))
 
-
-@app.post('/load_image/')
+@app.post('/image/')
 def load_image(image: SchemaImage):
     image_tags = tags()
     db_image = ModelImageInfo(url = image.url, tags = image_tags)
@@ -29,22 +27,22 @@ def load_image(image: SchemaImage):
     db.session.commit()
     return db_image
 
-@app.get('/get_image/')
+@app.get('/images/')
 def get_image():
     image = db.session.query(ModelImageInfo).all()
     return image
 
-@app.get('/get_image/{id}')
+@app.get('/image/')
 def get_image_id(id: int):
     image = db.session.query(ModelImageInfo).filter(ModelImageInfo.id == id).first()
     return image
 
-@app.get('/get_url_by_tags/')
+@app.get('/image/by-tags/')
 def get_url_by_tags(tags: str):
     image = db.session.query(ModelImageInfo).filter(ModelImageInfo.tags == tags).all()
     return image
 
-@app.get('/get_image_by_tags_and_id/')
+@app.get('/image/by-tags-and-id/')
 def get_image_by_tags_and_id(id: int, tags: str):
     image_dict = []
     image_id = db.session.query(ModelImageInfo).filter(ModelImageInfo.id == id).all()
@@ -53,10 +51,10 @@ def get_image_by_tags_and_id(id: int, tags: str):
     image_dict.append(image_tags)
     return image_dict
 
-@app.get('/get_id_by_tags/')
+@app.get('/image/id-by-tags/')
 def get_id_by_tags(tags: str):
-    image = db.session.query(ModelImageInfo).filter(ModelImageInfo.tags == tags).all()
-    return image
+    image_2 = db.session.query(ModelImageInfo).filter(ModelImageInfo.tags.ilike(f"%{tags}%")).all()
+    return image_2
 
 if __name__ == '__main__':
     uvicorn.run(app)
